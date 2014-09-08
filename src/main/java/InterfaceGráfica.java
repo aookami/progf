e922c3ce3f79bcc0c781b3dc0af379e1f5ada048
Bruno <Bruno@Bruno-PC>
2014-09-08 00:17:30 -0300

    parallel2

--- src/main/java/InterfaceGráfica.java
+++ src/main/java/InterfaceGráfica.java
@@ -140,7 +140,7 @@ public class InterfaceGráfica extends javax.swing.JFrame {
             cmd.timer.scheduleAtFixedRate(new ProcessCheck(cmd, Table,lista, model), 500, 500);
             //Criar uma tabela que cresce conforme é inserido conteudo.
             model.insertRow(model.getRowCount(), new Object[]{lista.get(0).cmd});
-            //Table.setValueAt(cmd.cmd, lista.size()-1, 0);           
+            Table.setValueAt(cmd.cmd, lista.size()-1, 0);           
             
             }
         }catch(IOException io){
